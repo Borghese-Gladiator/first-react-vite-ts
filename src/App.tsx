1f3d3658a2881d5ae6a343d3c97292f38b7b0692
itsumo_honeymoon_phase <timothy_shee@hotmail.com>
2024-02-24 14:26:15 -0500

    fix: app - style hovercard

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,10 @@ import {
 
 function App() {
   return (
-    
     <div className="flex justify-center  items-center h-screen">
       <HoverCard>
-        <HoverCardTrigger>First Shadcn Component</HoverCardTrigger>
-        <HoverCardContent>My first of many components</HoverCardContent>
+        <HoverCardTrigger className="rounded-xl text-white py-2 px-4 bg-slate-500">First Shadcn Component</HoverCardTrigger>
+        <HoverCardContent className=" font-bold text-slate-500 w-max">My first of many components</HoverCardContent>
       </HoverCard>
     </div>
   )
